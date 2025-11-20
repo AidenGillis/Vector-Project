894407d67e0622c129d37032c941a4ceccf52a60
AidenGillis <aidengillis.pro@gmail.com>
2025-11-19 19:28:43 -0500

    Option 1 and logistics setup

--- VectorProject_Python.py
+++ VectorProject_Python.py
@@ -1,14 +1,46 @@
+def Coord_to_degrees():
+    print("You have chosen to convert coordinates to decimal degrees. First we need some information.")
+    Lat_deg = float(input("Enter Latitude Degrees: "))
+    Lat_min = float(input("Enter Latitude Minutes: "))
+    Lat_sec = float(input("Enter Latitude Seconds: "))
+    Lon_deg = float(input("Enter Longitude Degrees: "))
+    Lon_min = float(input("Enter Longitude Minutes: "))
+    Lon_sec = float(input("Enter Longitude Seconds: "))
+    Lat_decimal = Lat_deg + (Lat_min / 60) + (Lat_sec / 3600)
+    Lon_decimal = Lon_deg + (Lon_min / 60) + (Lon_sec / 3600)
+    print(f"Decimal Latitude: {Lat_decimal}")
+    print(f"Decimal Longitude: {Lon_decimal}")
+    print("You better write those down cuz I'm not saving them for you!")
+
 print("Welcome! Type 'start' to start. :)")
 begin = input()
 
+while begin != "start":
+    print("Type 'start' to start")
+    begin = input()
 
 if begin == "start":
-    print("Pick one of the options below")
+    print("Pick one of the options below:")
     print("For Converting Coordinates to Degrees: Type '1'")
     print("For Resultant Vector using Vector Distance Formulas: Type '2'")
     print("For Resultant Vector using Haversine Formulas: Type '3'")
     print("For exiting the program: Type 'exit'")
     choice = input()
-else:
-    print("Type 'start' to start")
-    begin = input()
+
+    while choice not in ["1", "2", "3", "exit"]:
+        print("Invalid choice. Please pick one of the options below:")
+        print("For Converting Coordinates to Degrees: Type '1'")
+        print("For Resultant Vector using Vector Distance Formulas: Type '2'")
+        print("For Resultant Vector using Haversine Formulas: Type '3'")
+        print("For exiting the program: Type 'exit'")
+        choice = input()
+
+    if choice == "1":
+        Coord_to_degrees()
+    elif choice == "2":
+        Resultant_Vector_VDF()
+    elif choice == "3":
+        Resultant_Vector_HF()
+    elif choice == "exit":
+        print("Exiting the program. Goodbye :)")
+    
